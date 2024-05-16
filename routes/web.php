003ede95940ce914c0b5c506cf289df6a3a99d9e
uehata <uheijiuehata0725@gmail.com>
2024-05-16 15:15:30 +0900

    laravel breezeのインストールが終わって ./vendor/bin/sail npm install と./vendor/bin/sail npm run devをしたところ p112

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,7 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/tweet' , \App\Http\Controllers\Tweet\IndexController::class)
 ->name('tweet.index');
 Route::post('/tweet/create' , \App\Http\Controllers\Tweet\CreateController::class)
@@ -34,3 +34,15 @@ Route::delete('/tweet/delete/{tweetId}' , \App\Http\Controllers\Tweet\DeleteCont
 ->name('tweet.delete');
 Route::get('/tweet' , \App\Http\Controllers\Tweet\IndexController::class)
 ->name('tweet.index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
