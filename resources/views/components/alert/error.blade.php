b03cd33613f583c782cc5770a28ecbde2d53fc48
uehata <uheijiuehata0725@gmail.com>
2024-05-19 21:11:17 +0900

    p156

---
+++ resources/views/components/alert/error.blade.php
@@ -0,0 +1,6 @@
+<div class="w-full mt-1 mb-2 p-2 bg-red-500 item-center text-white leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+
+    <span class="font-semibold mr-2 text-left flex-auto pl-1">
+        {{$slot}}
+    </span>
+</div>
